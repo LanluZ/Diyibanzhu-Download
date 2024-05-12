481033c4d17462be7973dd2276e02159080ec4a1
LanluZ <LanluZ@qq.com>
2024-05-12 12:54:35 +0800

    1. 添加方法体

--- ocr/scr/analysis.py
+++ ocr/scr/analysis.py
@@ -3,6 +3,7 @@ import shutil
 from bs4 import BeautifulSoup
 
 
+# 解析删除多余标签
 def analysis_delete_tags(soup: BeautifulSoup):
     try:
         # 目标节点
@@ -27,6 +28,7 @@ def analysis_delete_tags(soup: BeautifulSoup):
     return soup
 
 
+# 解析添加字体文件
 def analysis_add_font(soup: BeautifulSoup, font_origin_path: str, font_aim_path: str):
     # css文件复制
     shutil.copy(font_origin_path, font_aim_path)
@@ -50,3 +52,13 @@ def analysis_add_font(soup: BeautifulSoup, font_origin_path: str, font_aim_path:
     soup.head.append(style_tag)
 
     return soup
+
+
+# 解析下载文字图片
+def analysis_download_img(soup: BeautifulSoup, source_url: str):
+    """
+    解析下载文字图片
+    :param soup: 下载得到的原始网页文件
+    :param source_url: 可供下载图片的网址,通常使用爬取该网页的地址
+    :return: 处理后的网页文件
+    """
