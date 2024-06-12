eff424eefb0009b15ee9991e2d02b68debf79658
LanluZ <LanluZ@qq.com>
2024-06-12 19:16:19 +0800

    1. 上传 poppler

--- ocr/main.py
+++ ocr/main.py
@@ -8,34 +8,53 @@ from pdf import convert_batch_pdf2image
 path = os.path.dirname(sys.argv[0])
 pdf_dir_path = os.path.join(path, 'data', 'pdf')  # pdf文件夹路径
 image_dir_path = os.path.join(path, 'data', 'image')  # 输出图片文件夹路径
+text_dir_path = os.path.join(path, 'data', 'text')  # 输出文本文件夹路径
 poppler_path = os.path.join(path, 'poppler', 'Library', 'bin')  # poppler路径
 
 
 def main():
-    # 初始化
-    exe_path = os.path.join(path, 'PaddleOCR-json', 'PaddleOCR-json.exe')  # 二进制程序路径
+    # pdf预处理
+    # convert_batch_pdf2image(pdf_dir_path, image_dir_path, poppler_path)
 
-    # 存在性判断
-    if os.path.exists(exe_path) is False:
-        print('> PaddleOCR-json 不存在 程序关闭!!!')
-        return
-    else:
-        print('> PaddleOCR-json 初始化进程开始')
+    # 图片载入
+    for image_dir_son_name in os.listdir(image_dir_path):
+        for image_name in os.listdir(os.path.join(image_dir_path, image_dir_son_name)):
+            image_path = os.path.join(image_dir_path, image_dir_son_name, image_name)
 
-    ocr = GetOcrApi(exe_path)
+            # 初始化
+            exe_path = os.path.join(path, 'PaddleOCR-json', 'PaddleOCR-json.exe')  # 二进制程序路径
+
+            # 存在性判断
+            if os.path.exists(exe_path) is False:
+                print('> PaddleOCR-json 不存在 程序关闭!!!')
+                return
+            else:
+                print('> PaddleOCR-json 初始化进程开始')
+
+            ocr = GetOcrApi(exe_path)
+
+            # 图片识别
+            res = ocr.run(image_path)
+            text_blocks = res['data']
+
+            # 识别结果
+            ocr.printResult(res)
+
+            # 识别结果可视化
+            visualize(text_blocks, image_path).save("保存.png")
+
+
+class Ocr:
+    # 初始化ocr
+    def __init__(self, exe_path):
+        self.ocr = GetOcrApi(exe_path)
+        self.res = None
 
     # 图片识别
-    img_path = os.path.join(path, 'test.png')
-    res = ocr.run(img_path)
-    text_blocks = res['data']
-
-    # 识别结果
-    ocr.printResult(res)
-
-    # 识别结果可视化
-    visualize(text_blocks, img_path).save("保存.png")
+    def run(self, image_path):
+        self.res = self.ocr.run(image_path)
+        return self.res
 
 
 if __name__ == '__main__':
     main()
-    # convert_batch_pdf2image(pdf_dir_path, image_dir_path, poppler_path)
