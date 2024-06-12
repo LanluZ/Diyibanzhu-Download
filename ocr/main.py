e214a04ca3fce7c98e8047fbccfeb591967ebb74
LanluZ <LanluZ@qq.com>
2024-06-12 16:56:15 +0800

    1. 上传 poppler工具

--- ocr/main.py
+++ ocr/main.py
@@ -1,5 +1,8 @@
-import os
 import sys
+import onnxruntime
+import cv2
+
+import numpy as np
 
 from scr.pdf import *
 
@@ -7,12 +10,11 @@ path = os.path.dirname(sys.argv[0])
 poppler_path = os.path.join(path, 'scr', 'poppler', 'Library', 'bin')
 data_pdf_path = os.path.join(path, 'data', 'pdf')
 data_image_path = os.path.join(path, 'data', 'image')
+data_model_path = os.path.join(path, 'model')
 
 
 def main():
-    # 批量转化pdf文件夹内所有pdf到image
-    convert_batch_pdf2image(data_pdf_path, data_image_path, poppler_path)
-    # 载入OCR模型
+
 
 
 if __name__ == '__main__':
