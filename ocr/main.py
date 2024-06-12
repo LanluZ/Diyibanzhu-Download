588bd8bb73c1109f27e183822af8296391da791a
LanluZ <LanluZ@qq.com>
2024-06-12 23:08:25 +0800

    1. 实现 OCR文本识别并保存文件

--- ocr/main.py
+++ ocr/main.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+import ocr.api.tbpu as tbpu
+
 from ocr.api.PPOCR_api import GetOcrApi
 from ocr.api.PPOCR_visualize import visualize
 from pdf import convert_batch_pdf2image
@@ -16,32 +18,42 @@ def main():
     # pdf预处理
     # convert_batch_pdf2image(pdf_dir_path, image_dir_path, poppler_path)
 
+    # 初始化
+    exe_path = os.path.join(path, 'PaddleOCR-json', 'PaddleOCR-json.exe')  # 二进制程序路径
+    # 存在性判断
+    if os.path.exists(exe_path) is False:
+        print('> PaddleOCR-json 不存在 程序关闭!!!')
+        return
+    else:
+        print('> PaddleOCR-json 初始化进程开始')
+    ocr = Ocr(exe_path)
+
     # 图片载入
     for image_dir_son_name in os.listdir(image_dir_path):
+
+        # 文本保存
+        text_output_file = open(os.path.join(text_dir_path, image_dir_son_name + '.txt'), 'w', encoding='utf-8')
+
+        print(f'> 子组：{image_dir_son_name} 开始识别')
+
+        # 子图识别
         for image_name in os.listdir(os.path.join(image_dir_path, image_dir_son_name)):
             image_path = os.path.join(image_dir_path, image_dir_son_name, image_name)
-
-            # 初始化
-            exe_path = os.path.join(path, 'PaddleOCR-json', 'PaddleOCR-json.exe')  # 二进制程序路径
-
-            # 存在性判断
-            if os.path.exists(exe_path) is False:
-                print('> PaddleOCR-json 不存在 程序关闭!!!')
-                return
-            else:
-                print('> PaddleOCR-json 初始化进程开始')
-
-            ocr = GetOcrApi(exe_path)
 
             # 图片识别
             res = ocr.run(image_path)
             text_blocks = res['data']
+            # 合并自然段
+            try:
+                text_blocks_new = tbpu.MergePara(text_blocks)
+            except AttributeError:
+                continue
 
-            # 识别结果
-            ocr.printResult(res)
+            # 文本保存
+            for text_line in text_blocks_new:
+                text_output_file.write(text_line['text'] + '\n')
 
-            # 识别结果可视化
-            visualize(text_blocks, image_path).save("保存.png")
+            print(f'> 子图：{image_name} 识别完成')
 
 
 class Ocr:
